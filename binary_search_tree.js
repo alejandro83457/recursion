e882bb8d8fc14ac501811f82ee297602803226ef
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-02-03 13:34:03 -0800

    Implement insert for BST

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -3,10 +3,10 @@ class Node {
   #left;
   #right;
 
-  constructor(data = undefined) {
+  constructor(data = null) {
     this.#data = data;
-    this.#left = undefined;
-    this.#right = undefined;
+    this.#left = null;
+    this.#right = null;
   }
 
   get data() {
@@ -37,14 +37,18 @@ class Node {
 class Tree {
   #root;
 
-  constructor(array) {
-    this.#root = this.buildTree(array, 0, array.length - 1);
+  constructor(array = []) {
+    // set root to null if array is empty to avoid undefined issues
+    if (array.length > 0)
+      this.#root = this.buildTree(array, 0, array.length - 1);
+    else this.#root = null;
   }
 
   get root() {
     return this.#root;
   }
 
+  // -----BUILD TREE-----
   buildTree(array, start, end) {
     // base case
     if (start > end) return null;
@@ -61,6 +65,32 @@ class Tree {
     return node;
   }
 
+  // -----INSERT VALUE-----
+  insert(value) {
+    console.log(`Inserting ${value}`);
+    this.#root = this.insertHelper(value, this.#root);
+  }
+  insertHelper(value, root) {
+    // base case will add node until it reaches correct leaf
+    if (root == null) {
+      console.log(`Current node is null`);
+      root = new Node(value);
+      root.setLeft(null);
+      root.setRight(null);
+      return root;
+    }
+    // choose left or right tree depending on less/greater
+    if (value < root.data) {
+      console.log(`${value} is less than ${root.data}`);
+      root.setLeft(this.insertHelper(value, root.left));
+    } else if (value > root.data) {
+      console.log(`${value} is greater than ${root.data}`);
+      root.setRight(this.insertHelper(value, root.right));
+    }
+    return root;
+  }
+
+  // -----VISUALIZATION-----
   // used to visualize the BST
   // provided by The Odin Project
   prettyPrint(node, prefix = '', isLeft = true) {
@@ -81,5 +111,8 @@ class Tree {
   }
 }
 
-let tree = new Tree([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+// let tree = new Tree([1, 2, 3, 4, 5, 7, 8, 9, 10]);
+let tree = new Tree();
+tree.prettyPrint(tree.root);
+tree.insert(6);
 tree.prettyPrint(tree.root);
